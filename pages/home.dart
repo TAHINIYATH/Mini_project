56139374f118d5ead1722a322c801dbfafcd04b3
TAHINIYATH <160420733064@mjcollege.ac.in>
2023-06-18 17:15:30 +0530

    new dart pages added

---
+++ pages/home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Intro extends StatelessWidget {
+  const Intro({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Column(
+        children: const [
+          Text(
+            'Home Page',
+            style: TextStyle(
+                fontSize: 40, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 100,
+          ),
+          CircleAvatar(
+            radius: 70,
+            child: Icon(
+              Icons.home,
+              size: 120,
+            ),
+          ),
+          SizedBox(
+            height: 100,
+          ),
+          Text(
+            'Let\'s introduce to Trash catch',
+            style: TextStyle(fontSize: 30, color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+}
